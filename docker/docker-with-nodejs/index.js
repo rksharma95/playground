600d369874ca95b31c1535a9e3c2d37574b4f67c
rksharma95 <nikhilrk13jan@gmail.com>
2022-03-31 15:19:12 +0530

    added mongo container

--- docker/docker-with-nodejs/index.js
+++ docker/docker-with-nodejs/index.js
@@ -1,6 +1,18 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config()
 
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./configs/config")
 const app = express();
+
+const MONGO_URI = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    })
+    .then(() => console.log("connected to database"))
+    .catch((e) => console.log(e))
 
 const PORT = process.env.PORT || 3000
 
